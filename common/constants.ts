1da244ce219172b220f9d08b111cd57e9673dae5
Siddhant Deshmukh <deshsid@amazon.com>
2024-12-23 00:22:40 -0800

    Query grouping dashboard changes and extensive tests
    Signed-off-by: Siddhant Deshmukh <deshsid@amazon.com>

--- common/constants.ts
+++ common/constants.ts
@@ -4,6 +4,9 @@
  */
 
 export const TIMESTAMP = 'Timestamp';
+export const TYPE = 'Type';
+export const QUERY_HASHCODE = 'Query Hashcode';
+export const QUERY_COUNT = 'Query Count';
 export const LATENCY = 'Latency';
 export const CPU_TIME = 'CPU Time';
 export const MEMORY_USAGE = 'Memory Usage';
@@ -11,3 +14,4 @@ export const INDICES = 'Indices';
 export const SEARCH_TYPE = 'Search type';
 export const NODE_ID = 'Coordinator node ID';
 export const TOTAL_SHARDS = 'Total shards';
+export const GROUP_BY = 'Group by';
